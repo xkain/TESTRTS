69e79819be8f749b09fb731a67663769b354aeb5
xkain <xkain@outlook.fr>
2026-06-09 15:20:27 +0200

    retest v2.5.6 s3  4 ou 8

--- src/GitOTA.cpp
+++ src/GitOTA.cpp
@@ -48,12 +48,10 @@ void GitRelease::setAssetProperty(const char *key, const char *val) {
       if(strlen(this->hwVersions)) strcat(this->hwVersions, ",");
       strcat(this->hwVersions, "wrover");
     }
-    // 🎯 Détection de la version 4MB
     else if(strstr(val, "ino.esp32s3_4mb.bin")) {
       if(strlen(this->hwVersions)) strcat(this->hwVersions, ",");
       strcat(this->hwVersions, "s3_4mb");
     }
-    // 🎯 Détection de la version 8MB
     else if(strstr(val, "ino.esp32s3_8mb.bin")) {
       if(strlen(this->hwVersions)) strcat(this->hwVersions, ",");
       strcat(this->hwVersions, "s3_8mb");
@@ -407,15 +405,11 @@ void GitUpdater::setFirmwareFile() {
 
   switch(ci.model) {
     case esp_chip_model_t::CHIP_ESP32S3: {
-      // 🎯 Détection dynamique de la taille de la Flash de l'ESP32-S3
-      // spi_flash_get_chip_size() retourne la taille en octets (ex: 4194304 pour 4MB)
       uint32_t flash_size = spi_flash_get_chip_size();
 
       if (flash_size >= 8 * 1024 * 1024) {
-        // Si la puce fait 8MB ou plus (comme ton XIAO)
         strlcpy(this->currentFile, "SomfyController.ino.esp32s3_8mb.bin", sizeof(this->currentFile));
       } else {
-        // Par défaut ou si la puce fait 4MB
         strlcpy(this->currentFile, "SomfyController.ino.esp32s3_4mb.bin", sizeof(this->currentFile));
       }
       break;
